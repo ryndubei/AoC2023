21d0b3fd0d9e235c9f0d4eaec74522e110b09a10
ryndubei <114586905+ryndubei@users.noreply.github.com>
2023-12-03 18:15:37 +0000

    Refactor Day 3

--- Solutions/Day3.hs
+++ Solutions/Day3.hs
@@ -2,60 +2,47 @@ module Solutions.Day3 (solution1, solution2) where
 
 import Data.List (groupBy)
 import Data.Char (isDigit, isSpace)
-import Control.Monad (guard)
+import Control.Monad (guard, mzero)
+import Text.Read (readMaybe)
 import Data.List.Extra (linesBy, nubOrd)
-import Control.Monad.State (execStateT, lift, modify)
 import qualified Data.Map.Strict as Map
 
 solution1 :: String -> Int
 solution1 s = sum $ do
-  l <- adjacents' $ linesPadded s
+  l <- adjacents $ linesPadded s
   num <- groupBy (\(a,_) (b,_) -> isDigit a && isDigit b) l
-  guard $ all (isDigit . fst) num
+  i <- maybe mzero pure (readMaybe (map fst num))
   guard $ any (any isSymbol' . snd) num
-  pure $ read (map fst num)
+  pure i
+  where
+    isSymbol' '.' = False
+    isSymbol' c = not (isDigit c || isSpace c)
 
 solution2 :: String -> Int
-solution2 s = sum $ Map.elems gearRatios
+solution2 s = sum 
+  . Map.map product 
+  . Map.filter ((==2) . length)
+  . Map.unionsWith (++) $ do
+      l <- adjacents $ linesBy (isSpace . snd) s'
+      num <- groupBy (\((_,a),_) ((_,b),_) -> isDigit a && isDigit b) l
+      i <- maybe mzero pure (readMaybe (map (snd . fst) num))
+      let numGears = nubOrd [fst n' | (_, n) <- num, n' <- n, n' `elem` gears]
+      pure . Map.fromList $ map (,[i]) numGears
   where
     s' = zip [0..] . unlines $ linesPadded s
     gears = filter ((=='*') . snd) s'
-    gearRatios = Map.map product
-      . Map.filter ((==2) . length) 
-      . Map.unionsWith (++) $ do
-        l <- adjacents' $ linesBy (isSpace . snd) s'
-        let gearRatios' = Map.unionsWith (++) . (`execStateT` mempty) $ do
-              num <- lift $ groupBy (\((_,a),_) ((_,b),_) -> isDigit a && isDigit b) l
-              guard $ all (isDigit . snd . fst) num
-              guard $ any (any (isSymbol' . snd) . snd) num
-              let numGears = nubOrd [fst n' | (_, n) <- num, n' <- n, n' `elem` gears]
-              let i = read $ map (snd . fst) num :: Int
-              mapM_ (\g -> modify (Map.insert g [i])) numGears
-        pure gearRatios'
 
 linesPadded :: String -> [String]
-linesPadded s =
-  let ls = lines s
-      n = length $ head ls
-      pad = replicate n '.'
-   in [pad] ++ map (\l -> "." ++ l ++ ".") ls ++ [pad]
+linesPadded s = ([pad] ++) . (++ [pad]) . map ((++ ".") . ("." ++)) $ ls
+  where
+    ls = lines s
+    pad = replicate (length $ head ls) '.'
 
-isSymbol' :: Char -> Bool
-isSymbol' '.' = False
-isSymbol' c = not (isDigit c || isSpace c)
-
-adjacents' :: [[a]] -> [[(a, [a])]]
-adjacents' ls = do
-  (prev, l, next) <- adjacents ls
-  let l' = adjacents l
-      prev' = adjacents prev
-      next' = adjacents next
+adjacents :: [[a]] -> [[(a, [a])]]
+adjacents ls = do
+  (prev, l, next) <- adj ls
   pure $ do
-    ((a,b,c),(d,e,f),(g,h,i)) <- zip3 l' prev' next'
+    ((a,b,c),(d,e,f),(g,h,i)) <- zip3 (adj l) (adj prev) (adj next)
     pure (b, [a,c,d,e,f,g,h,i])
-
-adjacents :: [a] -> [(a,a,a)]
-adjacents prevs =
-  let xs = tail prevs
-      nexts = tail xs
-   in zip3 prevs xs nexts
+  where
+    adj xs = zip3 xs (tail xs) (tail $ tail xs)
