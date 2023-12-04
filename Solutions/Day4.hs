9a620c876145b148a0b939707bfb917e3bbb3212
ryndubei <114586905+ryndubei@users.noreply.github.com>
2023-12-04 17:05:50 +0000

    Refactor Day 4

--- Solutions/Day4.hs
+++ Solutions/Day4.hs
@@ -9,36 +9,30 @@ import Data.MemoTrie (memo2)
 
 solution1 :: String -> Int
 solution1 s = sum $ do
-  l <- lines s
-  let l' = drop 10 l
-      (wins, haves) = second tail $ break (=='|') l'
-      wins' = map read $ words wins :: [Int]
-      haves' = map read $ words haves :: [Int]
-      matches = length $ intersect wins' haves'
+  (_,matches) <- Map.toList $ cardMap s
   pure $ case matches of
     0 -> 0
     n -> 2^(n - 1)
 
 solution2 :: String -> Int
 solution2 s = sum $ do
-  (i,l) <- Map.toList cards
-  go' i l
+  (i,matches) <- Map.toList cards
+  go' i matches
   where
     cards = cardMap s
     go' = memo2 go
-    go i l = do
-      let (wins, haves) = second tail $ break (=='|') l
-          wins' = map read $ words wins :: [Int]
-          haves' = map read $ words haves :: [Int]
-          matches = length $ wins' `intersect` haves'
-      1 : case matches of
-        0 -> []
-        _ -> do
-          (i', l') <- [ (idx, fromJust $ Map.lookup idx cards) | idx <- [i+1..i+matches]]
-          go' i' l'
+    go i matches = 1 : do
+      i' <- [i+1..i+matches]
+      let matches' = fromJust $ Map.lookup i' cards
+      go' i' matches'
 
-cardMap :: String -> Map Int String
+cardMap :: String -> Map Int Int
 cardMap s = Map.fromList $ do
   l <- lines s
-  let i = read . takeWhile (/=':') $ drop 5 l :: Int
-  pure (i, drop 10 l)
+  let i = read . takeWhile (/=':') $ drop 5 l
+      l' = drop 10 l
+      (wins, haves) = second tail $ break (=='|') l'
+      wins' = words wins
+      haves' = words haves
+      matches = length $ wins' `intersect` haves'
+  pure (i, matches)
